0a492b665839987d1bed6c2996afea25d2f3c0d4
Abhishek Singh Parihar <abhishek.p@webcodegenie.com>
2025-01-07 10:09:27 +0530

    Changes in docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,23 @@
-FROM node:20.9.0-alpine
+# Use a Node.js official image as the base image
+FROM node:18
 
-RUN apk add bash
+# Set the working directory in the container
+WORKDIR /app
 
-RUN npm install env-cmd -g
+# Copy package.json and package-lock.json (if exists)
+COPY package*.json ./
 
-WORKDIR /usr/src/app
-
-COPY package.json ./
-
+# Install dependencies, including devDependencies (for nodemon)
 RUN npm install
 
+# Copy the .env file into the Docker image (so it can be loaded by dotenv)
+COPY .env .env
+
+# Copy the rest of the application code
 COPY . .
+
+# Expose the port the app will run on
+EXPOSE 3000
+
+# Run the start script
+CMD ["sh", "start.sh"]
--- scripts/init.js
+++ scripts/init.js
@@ -5,42 +5,6 @@ const mongoose = require('mongoose');
 const userModel = require('../models/user');
 const saltedSha512 = require('salted-sha512');
 
-// (async () => {
-//     try {
-//         console.log("Init script execution start", process.env.DB);
-
-//         await mongoose.connect(process.env.DB);
-//         console.log("Database connected successfully");
-
-//         const admin = {
-//             name: "Super Admin",
-//             email: process.env.ADMIN_EMAIL,
-//             password: saltedSha512(process.env.ADMIN_PASSWORD, process.env.SHA512_SALT_KEY),
-//             mobile: process.env.ADMIN_MOBILE,
-//             phone:"0000000000",
-//             isAdmin: true
-//         };
-
-//         const isAdminExist = await userModel.countDocuments({ email: admin.email });
-
-//         if (!isAdminExist) {
-//             await userModel.create(admin);
-//             console.log("Admin Created !!");
-//         } else {
-//             console.log("Admin Already Exists");
-//         }
-
-//         console.log("Init Script Execution Done !!");
-
-//         process.exit(0);
-//     } catch (error) {
-//         console.error("Error while init script execution !!");
-//         console.log(error);
-//         process.exit(1);
-//     }
-// })();
-
-
 /**
  * Initial script to insert default data
  */
@@ -71,12 +35,10 @@ const saltedSha512 = require('salted-sha512');
             await mongoose.connect(dbURL);
 
             const admin = {
-                name: "Super Admin",
+                firstName: "Super",
+                lastName: " Admin",
                 email: process.env.ADMIN_EMAIL,
                 password: saltedSha512(process.env.ADMIN_PASSWORD, process.env.SHA512_SALT_KEY),
-                mobile: process.env.ADMIN_MOBILE,
-                phone:"0000000000",
-                isAdmin: true
             };
             await userModel.create(admin);
         }
